de79a5bb758af60c14f0cc89904cf125dd58ca27
Jason Lu <lu.json@icloud.com>
2014-07-01 14:53:22 -0400

    wip

---
+++ javascripts/models.js
@@ -0,0 +1,27 @@
+var CPU = Backbone.Model.extend({
+  
+  initialize: function() {
+    this.algorithm = "fifo";
+  }
+
+});
+
+var Algorith = Backbone.Model.extend({
+  initialize: function() {
+    this.name = "";
+  },
+  run: function(){
+
+  }
+  
+});
+
+var Process = Backbone.Model.extend({
+  initialize: function() {
+    arriveTime = 0;
+    burstTime = 0;
+    period = 0;
+    priority = 0;
+  }
+
+});
